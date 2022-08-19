f421219c5f38a6021c2a93379163ed9e7048bf65
Akash Ghosh <ag1229222@gmail.com>
2022-08-19 15:30:55 +0600

    A

--- lib/services.dart
+++ lib/services.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 
 class productServices {
   Future getProducts() async {
-    final productUrl = Uri.parse("https://fakestoreapi.com/products");
+    final productUrl = Uri.parse("http://localhost:5000/products");
     final response = await http.get(productUrl);
     return json.decode(response.body);
   }
